366c11568697441f61437769b8119d2e1c24bd1c
awillms20 <awillms17@gmail.com>
2024-10-17 17:58:39 -0400

    question1

---
+++ Decisions_Crossroad.py
@@ -0,0 +1,9 @@
+#Task 1 code Correction
+
+number= int(input("Enter a number"))
+if number > 0:
+    print("The number is positive.")
+elif number == 0:
+    print("The Number is zero")
+elif number < 0:
+    print("The number is negative.")
